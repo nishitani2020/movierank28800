10fdaa7f3802da12bb953423ced926ea1d648bdc
nishitani2020 <69030775+nishitani2020@users.noreply.github.com>
2020-10-16 18:49:35 +0900

    非同期実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,17 @@
 class LikesController < ApplicationController
+  before_action :movie_params
+
   def create
     Like.create(user_id: current_user.id, movie_id: params[:id])
-    redirect_to movie_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, movie_id: params[:id]).destroy
-    redirect_to movie_path
   end
 
+  private
+
+  def movie_params
+    @movie = Movie.find(params[:id])
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
-  validates :comment, presence: true
   belongs_to :user
   belongs_to :movie
   validates :comment, presence: true
